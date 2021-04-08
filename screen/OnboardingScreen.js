466b0ed73b03b3ade72b7bdd57b1629f2a8de186
geraldkachi <fitzgeraldkachi@gmail.com>
2021-04-08 09:08:06 +0100

    slider of onBoarding screen

--- screen/OnboardingScreen.js
+++ screen/OnboardingScreen.js
@@ -1,5 +1,12 @@
 import React from "react";
-import { Button, StyleSheet, Text, View, Image } from "react-native";
+import {
+  Button,
+  StyleSheet,
+  Text,
+  View,
+  Image,
+  TouchableOpacity,
+} from "react-native";
 import Onboarding from "react-native-onboarding-swiper";
 
 // import { Octicons } from "@expo/vector-icons";
@@ -8,22 +15,50 @@ const ImgIcon = require("../assets/icon.png");
 const ImgIconic = require("../assets/splash.png");
 
 const OnboardingScreen = ({ navigation }) => {
+  const Dots = ({ selected }) => (
+    <View
+      style={{
+        width: 5,
+        height: 5,
+        marginHorizontal: 3,
+        backgroundColor: selected ? "rgba(0,0,0,0.8)" : "rgba(0,0,0,0.3)",
+      }}
+    />
+  );
+
+  const Skip = ({ ...props }) => (
+    <Button title="Skip" color="#000" {...props} />
+  );
+  const Next = ({ ...props }) => (
+    <Button title="Next" color="#000" {...props} />
+  );
+
+  const Done = ({ ...props }) => (
+    <TouchableOpacity style={{ marginHorizontal: 10 }} {...props}>
+      <Text style={{ fontSize: 16 }}>Done</Text>
+    </TouchableOpacity>
+  );
+
   return (
     <Onboarding
-    onSkip={()=> navigation.navigate('BottomTab')}
-    onDone={()=> navigation.navigate('BottomTab')}
+      SkipButtonComponent={Skip}
+      NextButtonComponent={Next}
+      DoneButtonComponent={Done}
+      DotComponent={Dots}
+      onSkip={() => navigation.replace("BottomTab")}
+      onDone={() => navigation.navigate("BottomTab")}
       pages={[
         {
           backgroundColor: "#a6e4d0",
-          image: <Image source={ImgIcon}  />,
+          image: <Image source={ImgIcon} />,
           title: "Connect to the World",
           subtitle: "Done with React Native Swiper",
         },
         {
-            backgroundColor: "#fdeb93",
-            image: <Image source={ImgIconic} />,
-            title: "Share Your Fvaourite",
-            subtitle: "Done with React Native Swiper Second",
+          backgroundColor: "#fdeb93",
+          image: <Image source={ImgIconic} />,
+          title: "Share Your Fvaourite",
+          subtitle: "Done with React Native Swiper Second",
         },
         {
           backgroundColor: "#a9bcbe",
@@ -45,11 +80,11 @@ const styles = StyleSheet.create({
     alignItems: "center",
     justifyContent: "center",
   },
-//   imageslider: {
-//     justifyContent: "center",
-//     alignItems: "center",
-//     resizeMode: "cover",
-//   },
+  //   imageslider: {
+  //     justifyContent: "center",
+  //     alignItems: "center",
+  //     resizeMode: "cover",
+  //   },
 });
 
 //   npm audit fix
